702fe76f19f08f0be3e0d4ed9aed10fbd4d78b06
Dark-Brain07 <85172976+Dark-Brain07@users.noreply.github.com>
2026-03-27 22:11:06 +0600

    feat: add SparseSet module

---
+++ frontend/src/utils/structures/SparseSet.ts
@@ -0,0 +1,8 @@
+export class SparseSet<T=any>{private data:T[]=[];
+add(item:T){this.data.push(item);}
+remove():T|undefined{return this.data.pop();}
+peek():T|undefined{return this.data[this.data.length-1];}
+get size(){return this.data.length;}
+clear(){this.data=[];}
+toArray():T[]{return[...this.data];}
+isEmpty():boolean{return this.data.length===0;}}
