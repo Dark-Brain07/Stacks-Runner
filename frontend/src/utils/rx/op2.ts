5560eb90803b7f8702b45946ab0b6b99acb7ed71
Dark-Brain07 <85172976+Dark-Brain07@users.noreply.github.com>
2026-03-29 21:28:21 +0600

    refactor: add reactive operator set 2

---
+++ frontend/src/utils/rx/op2.ts
@@ -0,0 +1,3 @@
+export function map2<T,R>(fn:(v:T)=>R):(src:T[])=>R[]{return src=>src.map(fn);}
+export function filter2<T>(pred:(v:T)=>boolean):(src:T[])=>T[]{return src=>src.filter(pred);}
+export function reduce2<T,R>(fn:(acc:R,v:T)=>R,init:R):(src:T[])=>R{return src=>src.reduce(fn,init);}
