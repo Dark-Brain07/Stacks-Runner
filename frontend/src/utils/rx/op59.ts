8ec6e58a011258d9cb1fc1f2139cf8cc2850ec31
Dark-Brain07 <85172976+Dark-Brain07@users.noreply.github.com>
2026-03-29 21:28:48 +0600

    refactor: add reactive operator set 59

---
+++ frontend/src/utils/rx/op59.ts
@@ -0,0 +1,3 @@
+export function map59<T,R>(fn:(v:T)=>R):(src:T[])=>R[]{return src=>src.map(fn);}
+export function filter59<T>(pred:(v:T)=>boolean):(src:T[])=>T[]{return src=>src.filter(pred);}
+export function reduce59<T,R>(fn:(acc:R,v:T)=>R,init:R):(src:T[])=>R{return src=>src.reduce(fn,init);}
